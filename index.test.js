9ecbe72af85cffaf2d59ddb732834e3a5d14d1b3
jordanharris0 <jordanharris1833@yahoo.com>
2025-05-19 11:39:10 -0500

    finished tests

--- index.test.js
+++ index.test.js
@@ -1,22 +1,20 @@
 // install dependencies
-const { execSync } = require('child_process');
-execSync('npm install');
-execSync('npm run seed');
+const { execSync } = require("child_process");
+execSync("npm install");
+execSync("npm run seed");
 
-const request = require("supertest")
-const { db } = require('./db/connection');
-const { Musician } = require('./models/index')
-const app = require('./src/app');
-const {seedMusician} = require("./seedData");
-
-
-describe('./musicians endpoint', () => {
-    // Write your tests here
-    
-    
-
-
-
-
-    
-})
+const request = require("supertest");
+const { db } = require("./db/connection");
+const { Musician } = require("./models/index");
+const app = require("./src/app");
+const { seedMusician } = require("./seedData");
+describe("./musicians endpoint", () => {
+  // Write your tests here
+  test("GET /musicians", async () => {
+    const response = await request(app).get("/musicians");
+    const responseData = JSON.parse(response.text);
+    expect(response.statusCode).toBe(200);
+    expect(responseData.length).toBe(3);
+    expect(responseData[0].name).toBe("Mick Jagger");
+  });
+});
